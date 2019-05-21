e1c04750dea73b9f68a5b6d8976e1f24e6de9c13
never0land <49849926+never0land@users.noreply.github.com>
2019-05-21 19:43:32 +0800

    Add files via upload

--- airplaneList.js
+++ airplaneList.js
@@ -7,44 +7,37 @@ Page({
   data: {
     date: "",
     airplaneLists:[],
-    winHeight: 600,
+    winHeight: 20,
     currentTab: '1'
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (e) {
-    var trainNum = e.trainNum;
-    var trainDate = e.trainDate;
-    console.log("trainNum=" + trainNum +  "----trainDate=" + trainDate);
+    var trainNum =e.trainNum;
+    var trainDate = "2019-" + e.trainDate;
+    console.log(trainDate);
     wx.setNavigationBarTitle({
       title: "车次："+trainNum+"概况" 
     });
-    this.setData({ trainDate: trainDate });
- this.loadTrainsList(trainNum, trainNum);
+    this.setData({ trainDate: trainDate});
+    this.loadTrainsList(trainDate, trainNum);
   },
-  date: function (trainDate) {
-
-    for (var i = 0; i < trainDate.length; i++) {
-      trainDate.slice(i);
-    }
-  },
-  loadTrainsList: function (trainDate) {
-    //var page = this;
+  loadTrainsList: function (trainDate, trainNum) {
+    var page = this;
     var key = util.formatTime(new Date());
-    console.log(trainDate);
-    // console.log(key);
+    console.log(trainDate+"有问题");
+
     wx.request({
       url: 'http://route.showapi.com/909-2', //仅为示例，并非真实的接口地址
       data: {          //参数为json格式数据
         // img: '/public/img/1.png',
         showapi_sign: "5e2e8aaf845945e8bf8e439202a9958d",
-        showapi_appid: "95070",
-        //"to": to,
-        "trainDate": "2019-05-29",
+        showapi_appid: 95070,
+        "trainDate":"2019-05-30",
         "apiCode_pointCode": "909-2",
         "showapi_timestamp": "20190520100655",
-        "trainNum": "Z53",
+        "trainNum":trainNum,
         "testParam": "test"
       },
       header: {
@@ -56,11 +49,10 @@ Page({
         console.log("成功了")
         var airplaneLists = res.data.showapi_res_body.context;
         console.log(airplaneLists);
-      console.log(airplaneLists.context+"答案");
         var size = airplaneLists.length;
-        var winHeight = size * 100 + 30;
-       // page.setData({ airplaneLists: airplaneLists});
-       // page.setData({ winHeight: winHeight });
+        var winHeight = size * 5 + 30;
+        page.setData({ airplaneLists: airplaneLists});
+        page.setData({ winHeight: winHeight });
       }
     })
   },
